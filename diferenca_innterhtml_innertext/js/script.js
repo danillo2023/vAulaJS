e3be4d319e5bbc99213c5eebd71b4ca18f24faca
unknown <danwillgon@gmail.com>
2023-08-03 16:54:57 -0300

    att curso video aula JS

---
+++ diferenca_innterhtml_innertext/js/script.js
@@ -0,0 +1,10 @@
+// innerHTML - adicinando html e texto
+var contaier = document.querySelector("#contaier");
+
+contaier.innerHTML = "<h2> Testando o innerHTML</h2>";
+
+//innerText - adicionando o texto e nao em HTML
+var contaierB = document.querySelector("#contaierB");
+
+contaier_b.innerText = "<h2> Testando o innerText</h2>"
+
---
+++ exercicios/ax001.js
@@ -0,0 +1,14 @@
+var pao = document.querySelector('#input1')
+var arroz = document.querySelector('#input2')
+var feijao = document.querySelector('#input3')
+
+function calcularProdutos () {
+    pao = parseFloat(pao.value)
+    arroz = parseFloat(arroz.value)
+    feijao = parseFloat(feijao.value)
+
+    var soma = pao + arroz + feijao
+
+    alert(soma)
+   
+}
